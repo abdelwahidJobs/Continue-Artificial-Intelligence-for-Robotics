d00356bb616c29f8c724f1f316feb07a833644a2
abdelwahidJobs <abdoo.max@gmail.com>
2016-06-17 02:12:46 +0200

    end of particle filters

--- robot.py
+++ robot.py
@@ -111,24 +111,51 @@ def eval(r, p):
     return sum / float(len(p))
 
 
-
-####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER CODE BELOW ####
-N= 1000
-P=[]
-for i in range(N):
-	x = robot()
-	P.append(x)
-print len(P)
-P2=[]
-for i in range(N):
-	P2.append(P[i].move(0.1,5.0))
-
-P=P2
-
+def eval(r, p):
+    sum = 0.0;
+    for i in range(len(p)): # calculate mean error
+        dx = (p[i].x - r.x + (world_size/2.0)) % world_size - (world_size/2.0)
+        dy = (p[i].y - r.y + (world_size/2.0)) % world_size - (world_size/2.0)
+        err = sqrt(dx * dx + dy * dy)
+        sum += err
+    return sum / float(len(p))
+####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER/MODIFY CODE BELOW ####
 myrobot = robot()
-myrobot.set_noise(5.00,0.1,5.0)
-myrobot.set(30.0 , 50.0 , pi/2)
-myrobot = myrobot.move(-pi/2,15.0)
-print myrobot.sense()
-myrobot = myrobot.move(-pi/2 , 10.0 )
-print myrobot.sense()
+myrobot = myrobot.move(0.1, 5.0)
+Z = myrobot.sense()
+N = 1000
+T = 10 #Leave this as 10 for grading purposes.
+
+p = []
+for i in range(N):
+    r = robot()
+    r.set_noise(0.05, 0.05, 5.0)
+    p.append(r)
+
+for t in range(T):
+    myrobot = myrobot.move(0.1, 5.0)
+    Z = myrobot.sense()
+
+    p2 = []
+    for i in range(N):
+        p2.append(p[i].move(0.1, 5.0))
+    p = p2
+
+    w = []
+    for i in range(N):
+        w.append(p[i].measurement_prob(Z))
+
+    p3 = []
+    index = int(random.random() * N)
+    beta = 0.0
+    mw = max(w)
+    for i in range(N):
+        beta += random.random() * 2.0 * mw
+        while beta > w[index]:
+            beta -= w[index]
+            index = (index + 1) % N
+        p3.append(p[index])
+    p = p3
+    #enter code here, make sure that you output 10 print statementsself.
+    print eval(myrobot,p)
+
